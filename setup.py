d9d2f053b3a66f12755cf2e4d8e823b552d87fdb
ajlee21 <alexjlee.21@gmail.com>
2020-06-14 12:33:38 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ponyo',
+version='0.1',
+description='Install functions from ponyo: simulate gene expression compendia',
+url='#',
+author='Alexandra Lee',
+author_email='alexjlee.21@gmail.com',
+license='BSD 3-Clause',
+packages=['ponyo'],
+zip_safe=False)
