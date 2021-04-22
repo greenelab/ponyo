b80a892699d21dd5071d61d9fd6e564395ab352a
ajlee21 <alexjlee.21@gmail.com>
2021-04-22 15:34:31 -0400

    revert tf version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "pandas",
         "numpy",
         "keras==2.3.1",
-        "tensorflow==2.0.0",
+        "tensorflow==1.15.4",
         "scikit-learn",
         "h5py<3",
     ],
