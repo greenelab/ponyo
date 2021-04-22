3016e46b22918238991e052ff83e3f3abb87622d
ajlee21 <alexjlee.21@gmail.com>
2021-04-22 15:19:40 -0400

    fix specifier for py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     license="BSD 3-Clause",
     packages=["ponyo"],
     zip_safe=False,
-    python_requires="=3.5, <3.8",
+    python_requires=">=3.5, <3.8",
     install_requires=[
         "pandas",
         "numpy",
