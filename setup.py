070bf7f6b59f7faae33d83fd7960a212c0852de7
Ben Heil <ben.jer.heil@gmail.com>
2021-06-24 10:38:37 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras = {
 
 setup(
     name="ponyo",
-    version="0.6",
+    version="0.6.1",
     description="Install functions to simulate gene expression compendia",
     long_description=README,
     long_description_content_type="text/markdown",
